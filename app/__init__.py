6a38b4a801c4c9d3dac4ecf3d5108bc7181c1cde
lfbrandt <luisbdev@hotmail.com>
2025-07-08 10:41:36 -0300

    Update CSP script-src

--- app/__init__.py
+++ app/__init__.py
@@ -63,7 +63,7 @@ def create_app():
     # Configurar políticas de segurança HTTP com Flask-Talisman
     csp = {
         'default-src': ["'self'"],
-        'script-src': ["'self'"],
+        'script-src': ["'self'", 'https://cdn.jsdelivr.net'],
         'style-src': ["'self'", 'https://fonts.googleapis.com'],
         'img-src': ["'self'", 'data:'],
         'font-src': ["'self'", 'https://fonts.gstatic.com'],
